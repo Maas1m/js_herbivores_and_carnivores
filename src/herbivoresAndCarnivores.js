e2ff3f6c258ad4f99dc0f2aae12c2b92096322f4
Krot Max <frotmas1m@gmail.com>
2024-11-19 20:45:25 +0200

    Solutoin

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,55 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static removeDied(animal) {
+    const index = Animal.alive.indexOf(animal);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+
+    if (this.health <= 0) {
+      Animal.removeDied(this);
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      const damage = 50;
+
+      target.takeDamage(damage);
+    }
+  }
 }
 
 module.exports = {
